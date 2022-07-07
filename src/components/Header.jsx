88d59ced160f1cb56c1fc263629d6bdd8d79ddf6
stillwannawork <denistillwannawork@gmail.com>
2022-07-07 20:25:19 +0500

    full project from notebook

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png'></img>
+        </header>
+    )
+}
+
+export default Header;
